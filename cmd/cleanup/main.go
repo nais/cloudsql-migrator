726180cd47e7c27ef3f080ea2bf1d8c752957e18
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-05-23 16:04:20 +0200

    Make main methods the same

--- cmd/cleanup/main.go
+++ cmd/cleanup/main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/nais/cloudsql-migrator/internal/pkg/common_main"
 	"github.com/nais/cloudsql-migrator/internal/pkg/config"
 	"github.com/sethvargo/go-envconfig"
 	"os"
 )
 
 func main() {
-	var conf struct {
-		config.CommonConfig
-	}
+	cfg := config.CommonConfig{}
 
 	ctx := context.Background()
 
-	if err := envconfig.Process(ctx, &conf); err != nil {
+	if err := envconfig.Process(ctx, &cfg); err != nil {
 		fmt.Printf("Invalid configuration: %v", err)
-		os.Exit(125)
+		os.Exit(1)
 	}
 
-	logger := config.SetupLogging(&conf.CommonConfig)
-	logger.Info("Cleanup started", "config", conf)
+	logger := config.SetupLogging(&cfg)
+	mgr, err := common_main.Main(ctx, &cfg, logger)
+	if err != nil {
+		logger.Error("failed to complete configuration", "error", err)
+		os.Exit(2)
+	}
+
+	mgr.Logger.Info("cleanup started", "config", cfg)
 }
--- cmd/promote/main.go
+++ cmd/promote/main.go
@@ -17,19 +17,17 @@ func main() {
 
 	if err := envconfig.Process(ctx, &cfg); err != nil {
 		fmt.Printf("invalid configuration: %v", err)
-		os.Exit(125)
+		os.Exit(1)
 	}
 
 	logger := config.SetupLogging(&cfg)
-	logger.Info("promote started", "config", cfg)
-
 	mgr, err := common_main.Main(ctx, &cfg, logger)
 	if err != nil {
 		logger.Error("failed to complete configuration", "error", err)
 		os.Exit(2)
 	}
 
-	mgr.Logger.Info("setup started", "config", cfg)
+	mgr.Logger.Info("promote started", "config", cfg)
 
 	err = promote.Promote(ctx, &cfg, mgr)
 	if err != nil {
