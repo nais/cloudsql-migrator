23b19bd81746000955a1e3f3c6d0f85655e04e0f
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-05-23 13:56:08 +0200

    Move resolving functions to resolved package
    Co-authored-by: Sten Røkke <sten.ivar.rokke@nav.no>

--- internal/pkg/common_main/main.go
+++ internal/pkg/common_main/main.go
@@ -11,13 +11,11 @@ import (
 	naisv1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
 	"google.golang.org/api/datamigration/v1"
 	"google.golang.org/api/sqladmin/v1"
-	core_v1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"log/slog"
-	"strings"
 )
 
 type Manager struct {
@@ -82,14 +80,14 @@ func Main(ctx context.Context, cfg *config.CommonConfig, logger *slog.Logger) (*
 	}, nil
 }
 
-func resolveClusterInformation(ctx context.Context, cfg *config.CommonConfig, clientset kubernetes.Interface, client k8s.AppClient, sqlInstanceClient k8s.SqlInstanceClient, resolved *resolved.Resolved) error {
+func resolveClusterInformation(ctx context.Context, cfg *config.CommonConfig, clientset kubernetes.Interface, client k8s.AppClient, sqlInstanceClient k8s.SqlInstanceClient, r *resolved.Resolved) error {
 	ns, err := clientset.CoreV1().Namespaces().Get(ctx, cfg.Namespace, v1.GetOptions{})
 	if err != nil {
 		return err
 	}
 
 	if projectId, ok := ns.Annotations["cnrm.cloud.google.com/project-id"]; ok {
-		resolved.GcpProjectId = projectId
+		r.GcpProjectId = projectId
 	} else {
 		return fmt.Errorf("unable to determine google project id for namespace %s", cfg.Namespace)
 	}
@@ -99,80 +97,38 @@ func resolveClusterInformation(ctx context.Context, cfg *config.CommonConfig, cl
 		return fmt.Errorf("unable to get existing application: %w", err)
 	}
 
-	resolved.Source.Name, err = resolveInstanceName(app)
+	err = r.ResolveSourceInstanceName(app)
 	if err != nil {
 		return err
 	}
 
-	resolved.DatabaseName, err = resolveDatabaseName(app)
+	err = r.ResolveDatabaseName(app)
 	if err != nil {
 		return err
 	}
 
-	resolved.Source.AppUsername = resolveAppUsername(app)
+	r.ResolveSourceAppUsername(app)
 
 	secret, err := clientset.CoreV1().Secrets(cfg.Namespace).Get(ctx, "google-sql-"+cfg.ApplicationName, v1.GetOptions{})
 	if err != nil {
 		return err
 	}
-	resolved.Source.AppPassword, err = resolveAppPassword(secret)
+
+	err = r.ResolveSourceAppPassword(secret)
 	if err != nil {
 		return err
 	}
 
-	sqlInstance, err := sqlInstanceClient.Get(ctx, resolved.Source.Name)
+	sqlInstance, err := sqlInstanceClient.Get(ctx, r.Source.Name)
 	if err != nil {
 		return fmt.Errorf("unable to get existing sql instance: %w", err)
 	}
 	if sqlInstance.Status.PublicIpAddress == nil {
-		return fmt.Errorf("sql instance %s does not have public ip address", resolved.Source.Name)
+		return fmt.Errorf("sql instance %s does not have public ip address", r.Source.Name)
 	}
-	resolved.Source.Ip = *sqlInstance.Status.PublicIpAddress
+	r.Source.Ip = *sqlInstance.Status.PublicIpAddress
 
 	return nil
-}
-
-func resolveAppPassword(secret *core_v1.Secret) (string, error) {
-	for key, bytes := range secret.Data {
-		if strings.HasSuffix(key, "_PASSWORD") {
-			return string(bytes), nil
-		}
-	}
-	return "", fmt.Errorf("unable to find password in secret %s", secret.Name)
-}
-
-func resolveAppUsername(app *naisv1alpha1.Application) string {
-	return app.ObjectMeta.Name
-}
-
-func resolveInstanceName(app *naisv1alpha1.Application) (string, error) {
-	spec := app.Spec
-	if spec.GCP != nil {
-		gcp := spec.GCP
-		if gcp.SqlInstances != nil && len(gcp.SqlInstances) == 1 {
-			instance := gcp.SqlInstances[0]
-			if len(instance.Name) > 0 {
-				return instance.Name, nil
-			}
-			return app.ObjectMeta.Name, nil
-		}
-	}
-	return "", fmt.Errorf("application does not have sql instance")
-}
-
-func resolveDatabaseName(app *naisv1alpha1.Application) (string, error) {
-	spec := app.Spec
-	if spec.GCP != nil {
-		gcp := spec.GCP
-		if gcp.SqlInstances != nil && len(gcp.SqlInstances) == 1 {
-			database := gcp.SqlInstances[0].Databases[0]
-			if len(database.Name) > 0 {
-				return database.Name, nil
-			}
-			return app.ObjectMeta.Name, nil
-		}
-	}
-	return "", fmt.Errorf("application does not have sql database")
 }
 
 func newK8sClient() (kubernetes.Interface, dynamic.Interface, error) {
--- internal/pkg/resolved/resolved.go
+++ internal/pkg/resolved/resolved.go
@@ -1,6 +1,11 @@
 package resolved
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
+	"k8s.io/api/core/v1"
+	"strings"
+)
 
 // Resolved is configuration that is resolved by looking up in the cluster
 
@@ -32,4 +37,52 @@ func (r *Resolved) GcpParentURI() string {
 
 func (r *Resolved) GcpComponentURI(kind, name string) string {
 	return fmt.Sprintf("%s/%s/%s", r.GcpParentURI(), kind, name)
+}
+
+func (r *Resolved) ResolveSourceAppPassword(secret *v1.Secret) error {
+	for key, bytes := range secret.Data {
+		if strings.HasSuffix(key, "_PASSWORD") {
+			r.Source.AppPassword = string(bytes)
+			return nil
+		}
+	}
+	return fmt.Errorf("unable to find password in secret %s", secret.Name)
+}
+
+func (r *Resolved) ResolveSourceAppUsername(app *nais_io_v1alpha1.Application) {
+	r.Source.AppUsername = app.ObjectMeta.Name
+}
+
+func (r *Resolved) ResolveSourceInstanceName(app *nais_io_v1alpha1.Application) error {
+	spec := app.Spec
+	if spec.GCP != nil {
+		gcp := spec.GCP
+		if gcp.SqlInstances != nil && len(gcp.SqlInstances) == 1 {
+			instance := gcp.SqlInstances[0]
+			if len(instance.Name) > 0 {
+				r.Source.Name = instance.Name
+				return nil
+			}
+			r.Source.Name = app.ObjectMeta.Name
+			return nil
+		}
+	}
+	return fmt.Errorf("application does not have sql instance")
+}
+
+func (r *Resolved) ResolveDatabaseName(app *nais_io_v1alpha1.Application) error {
+	spec := app.Spec
+	if spec.GCP != nil {
+		gcp := spec.GCP
+		if gcp.SqlInstances != nil && len(gcp.SqlInstances) == 1 && len(gcp.SqlInstances[0].Databases) == 1 {
+			database := gcp.SqlInstances[0].Databases[0]
+			if len(database.Name) > 0 {
+				r.DatabaseName = database.Name
+				return nil
+			}
+			r.DatabaseName = app.ObjectMeta.Name
+			return nil
+		}
+	}
+	return fmt.Errorf("application does not have sql database")
 }
