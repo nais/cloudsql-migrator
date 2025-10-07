a2176214717bd9595fcc302c2c7d723e1be7a6ac
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-10-07 14:39:40 +0200

    Scale down application if lag is reasonable, promote when lag is zero
    Split the replication lag check in two parts.
    
    The first part runs while the application is still running, and will accept a reasonable lag (16MB) and allow the promote to continue.
    
    The second runs after the application has been scaled to 0, and will only allow promotion to proceed when a lag of 0 bytes is confirmed.

--- internal/pkg/promote/promote.go
+++ internal/pkg/promote/promote.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
 	"time"
 
+	"github.com/sethvargo/go-retry"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -19,6 +21,13 @@ import (
 	"google.golang.org/api/datamigration/v1"
 	"google.golang.org/api/iterator"
 )
+
+const (
+	numberOfZeroPointsForLagToBeConsideredZero = 3
+	acceptableLagBytesForPromotion             = 16 * 1024 * 1024
+)
+
+type ReplicationLagPredicate func([]*monpb.Point, *slog.Logger) (bool, error)
 
 func CheckReadyForPromotion(ctx context.Context, source, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
 	migrationName, err := resolved.MigrationName(source.Name, target.Name)
@@ -51,15 +60,15 @@ func CheckReadyForPromotion(ctx context.Context, source, target *resolved.Instan
 		return fmt.Errorf("migration job is not ready for promotion: %s", migrationJob.Phase)
 	}
 
-	err = waitForReplicationLagToReachZero(ctx, target, gcpProject, mgr)
+	return waitForReplicationLagToBeAcceptablyLow(ctx, target, gcpProject, mgr)
+}
+
+func Promote(ctx context.Context, source, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
+	err := waitForReplicationLagToReachZero(ctx, target, gcpProject, mgr)
 	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func Promote(ctx context.Context, source, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
 	migrationName, err := resolved.MigrationName(source.Name, target.Name)
 	if err != nil {
 		return err
@@ -89,19 +98,112 @@ func Promote(ctx context.Context, source, target *resolved.Instance, gcpProject 
 	return nil
 }
 
-func waitForReplicationLagToReachZero(ctx context.Context, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
-	ctx, cancel := context.WithTimeout(ctx, 6*time.Minute)
-	defer cancel()
+func waitForReplicationLagToBeAcceptablyLow(ctx context.Context, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
+	return waitForReplicationLag(ctx, target, lagAcceptablyLow, gcpProject, mgr)
+}
 
+func waitForReplicationLagToReachZero(ctx context.Context, target *resolved.Instance, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
+	return waitForReplicationLag(ctx, target, lagReachedZero, gcpProject, mgr)
+}
+
+func waitForReplicationLag(ctx context.Context, target *resolved.Instance, predicate ReplicationLagPredicate, gcpProject *resolved.GcpProject, mgr *common_main.Manager) error {
 	client, err := monitoring.NewMetricClient(ctx)
 	if err != nil {
 		panic(fmt.Sprintf("failed to create metric client: %v", err))
 	}
 	defer client.Close()
 
+	b := retry.NewConstant(30 * time.Second)
+	b = retry.WithMaxDuration(10*time.Minute, b)
+
+	return retry.Do(ctx, b, func(ctx context.Context) error {
+		req := makeMetricsRequest(gcpProject, target)
+
+		mgr.Logger.Info("checking replication lag")
+		it := client.ListTimeSeries(ctx, req)
+		data, err := it.Next()
+		if err != nil {
+			if !errors.Is(err, iterator.Done) {
+				return retry.RetryableError(fmt.Errorf("failed to fetch time series data: %w", err))
+			}
+			mgr.Logger.Info("no more data in iterator, retrying")
+			return retry.RetryableError(fmt.Errorf("no more data in iterator"))
+		}
+
+		mgr.Logger.Debug("fetched time series data", "number_of_points", len(data.Points))
+		result, err := predicate(data.Points, mgr.Logger)
+		if err != nil {
+			return retry.RetryableError(fmt.Errorf("failed to evaluate predicate: %w", err))
+		}
+		if !result {
+			return retry.RetryableError(fmt.Errorf("predicate not satisfied, retrying"))
+		}
+		return nil
+	})
+}
+
+func lagAcceptablyLow(points []*monpb.Point, logger *slog.Logger) (bool, error) {
+	if len(points) == 0 {
+		logger.Debug("no data points available to determine lag")
+		return false, nil
+	}
+
+	point := points[0]
+
+	value, err := getPointValue(point)
+	if err != nil {
+		logger.Warn("failed to get point value", "error", err)
+		return false, err
+	}
+	logger.Debug("lag", "value", value, "point", formatPoint(point))
+
+	if value > acceptableLagBytesForPromotion {
+		logger.Debug("lag is too high for promotion", "lag_bytes", value, "acceptable_lag_bytes", acceptableLagBytesForPromotion)
+		return false, nil
+	}
+
+	logger.Info("lag is acceptably low for promotion", "lag_bytes", value)
+	return true, nil
+}
+
+func lagReachedZero(points []*monpb.Point, logger *slog.Logger) (bool, error) {
+	if len(points) < numberOfZeroPointsForLagToBeConsideredZero {
+		logger.Debug("not enough data points to determine if lag has reached zero", "points_available", len(points), "points_required", numberOfZeroPointsForLagToBeConsideredZero)
+		return false, nil
+	}
+
+	for _, point := range points[0:numberOfZeroPointsForLagToBeConsideredZero] {
+		value, err := getPointValue(point)
+		if err != nil {
+			logger.Warn("failed to get point value", "error", err)
+			return false, err
+		}
+		logger.Debug("lag", "value", value, "point", formatPoint(point))
+
+		if value != 0 {
+			return false, nil
+		}
+	}
+
+	logger.Info("lag has reached zero")
+	return true, nil
+}
+
+func getPointValue(point *monpb.Point) (int64, error) {
+	value := point.GetValue()
+	if value == nil {
+		return 0, fmt.Errorf("point has no value")
+	}
+	intValue, ok := value.GetValue().(*monpb.TypedValue_Int64Value)
+	if !ok {
+		return 0, fmt.Errorf("point value is not int64")
+	}
+	return intValue.Int64Value, nil
+}
+
+func makeMetricsRequest(gcpProject *resolved.GcpProject, target *resolved.Instance) *monpb.ListTimeSeriesRequest {
 	endTime := time.Now()
 	startTime := endTime.Add(-5 * time.Minute)
-
 	req := &monpb.ListTimeSeriesRequest{
 		Name:   "projects/" + gcpProject.Id,
 		Filter: fmt.Sprintf(`metric.type="cloudsql.googleapis.com/database/postgresql/external_sync/max_replica_byte_lag" AND resource.labels.region="%s" AND resource.labels.project_id="%s" AND resource.labels.database_id="%s:%s"`, target.Region, gcpProject.Id, gcpProject.Id, target.Name),
@@ -115,33 +217,19 @@ func waitForReplicationLagToReachZero(ctx context.Context, target *resolved.Inst
 			PerSeriesAligner: monpb.Aggregation_ALIGN_MAX,
 		},
 	}
+	return req
+}
 
-	for {
-		mgr.Logger.Info("checking replication lag")
-		it := client.ListTimeSeries(ctx, req)
-		replicationLagZero := true
-		data, err := it.Next()
-		if err != nil {
-			if !errors.Is(err, iterator.Done) {
-				return fmt.Errorf("failed to fetch time series data: %w", err)
-			}
-			mgr.Logger.Info("no more data in iterator")
-		} else {
-			for _, point := range data.Points {
-				switch value := point.GetValue().GetValue().(type) {
-				case *monpb.TypedValue_Int64Value:
-					if value.Int64Value != 0 {
-						replicationLagZero = false
-						time.Sleep(30 * time.Second)
-					}
-				default:
-					return fmt.Errorf("unknown type from cloud monitoring: %T", value)
-				}
-			}
-			if replicationLagZero {
-				mgr.Logger.Info("replication lag reached zero")
-				return nil
-			}
-		}
+func formatPoint(point *monpb.Point) string {
+	interval := point.GetInterval()
+	if interval == nil {
+		return "no interval"
 	}
+	start := formatTimestamp(interval.GetStartTime())
+	end := formatTimestamp(interval.GetEndTime())
+	return fmt.Sprintf("start: %s, end: %s", start, end)
+}
+
+func formatTimestamp(t *timestamppb.Timestamp) string {
+	return t.AsTime().Format(time.RFC3339)
 }
